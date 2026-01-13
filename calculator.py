ced53d360f606a9b81d192fe5e3c7fcfb56d2ee2
Rukayya222 <rukayyagaya21@gmail.com>
2026-01-13 06:42:01 +0100

    feat: Initial release of the calculator with add function

---
+++ calculator.py
@@ -0,0 +1,7 @@
+def add(a, b):
+    """This function adds two numbers"""
+    return a + b
+
+print("Welcome to the Simple Calculator!")
+print("Version: 1.0.0")
+print("Result of add(2, 2) is:", add(2, 2))
