1bcefbcaf6d4d9d2c159a5a2c391ebab7e5cf1f0
Rukayya222 <rukayyagaya21@gmail.com>
2026-01-13 07:06:48 +0100

    fix: Updated welcome message for clarity

--- calculator.py
+++ calculator.py
@@ -2,6 +2,6 @@ def add(a, b):
     """This function adds two numbers"""
     return a + b
 
-print("Welcome to the Simple Calculator!")
-print("Version: 1.0.0")
+print("Welcome to the Super Simple Calculator!")
+print("Version: 1.0.1")
 print("Result of add(2, 2) is:", add(2, 2))
