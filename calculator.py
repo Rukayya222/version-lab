8baa6edc9b3e87bf74f696b4b10bc83d35eed9d3
Rukayya222 <rukayyagaya21@gmail.com>
2026-01-13 07:17:16 +0100

    feat: Added subtract function

--- calculator.py
+++ calculator.py
@@ -1,7 +1,11 @@
 def add(a, b):
-    """This function adds two numbers"""
     return a + b
 
+def subtract(a, b):
+    return a - b
+
 print("Welcome to the Super Simple Calculator!")
-print("Version: 1.0.1")
+print("Version: 1.1.0")
 print("Result of add(2, 2) is:", add(2, 2))
+print("Result of subtract(5, 3) is:", subtract(5, 3))
+
